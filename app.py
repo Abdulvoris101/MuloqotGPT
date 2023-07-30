c3de1fb92305cbc892235d0b244ae9588f7b5051
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-07-30 20:13:30 +0500

    CHANGE: changed port

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ add_pagination(app)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=False, workers=1)
+    uvicorn.run("app:app", host='0.0.0.0', port=8005, reload=False, workers=1)
