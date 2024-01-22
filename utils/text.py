59e8400c6a594466d772fa8428c640d0d664d6e1
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2024-01-22 14:12:10 +0500

    Changed command texts

--- utils/text.py
+++ utils/text.py
@@ -5,51 +5,26 @@ from utils import constants
 COST = constants.AQSHA_COST
 
 START_COMMAND = """🤖 Salom! Men MuloqotAi, sizning shaxsiy 
-yordamchingizman.
-Avtotarjimon yoniq xolatda.
-Batafsil ma'lumot uchun - /help
+yordamchingizman. Foydalanish uchun shunchaki savolingizni botga yozish kifoya.
+
+Buyruqlar:
+/start - Botni qayta ishga tushirish
+/art - rasm generatsiya qilish
+/settings - Avtotarjimon sozlamasi
+/premium - Premium obuna
+/help - Foydalanish qo’llanmasi
 """
 
 HOW_TO_HELP_TEXT = """Sizga qanday yordam bera olaman?"""
 
 HELP_COMMAND = """<b>Botni qanday ishlataman?</b>
 Botda  chatgptni  ishlatish uchun botga shunchaki so'rov yuborish kifoya. 
-Rasm generatsiyasi uchun esa shu komandani yuboring: /art prompt. Prompt o'rniga o'zingizni so'rovingizni yuboring. 
+Rasm generatsiyasi uchun esa ushbu komandani yuboring: /art prompt. Prompt o'rniga o'zingizni so'rovingizni yuboring. 
 
 <b>Botning qo'shimcha xususiyatlari</b>:
-🔹<b>Avtotarjima:</b> - bilasiz chatgpt o'zbek tilini tushunmaydi shuning uchun botda avtotarjima xususiyati mavjud, agarda avtotarjimani yoqib qo'ysangiz sizning xar bir so'rovingiz tarjimon orqali ingliz tilga  o'tqizilib chatgptga yuboriladi va  kelgan javob esa o'zbekchaga tarjima qilinadi. Bu bilan siz ingliz tilini bilmasdan turib chatgptni to'liqona ishlatishingiz mumkin bo'ladi.
+🔹<b>Avtotarjima:</b> - bilasiz chatgpt o'zbek tilini  unchalik yahshi tushunmaydi shuning uchun botda avtotarjima xususiyati mavjud, agarda avtotarjimani yoqib qo'ysangiz sizning xar bir so'rovingiz  ingliz tilga  o'tqizilib chatgptga yuboriladi va  kelgan javob esa o'zbekchaga tarjima qilinadi. Bu bilan siz ingliz tilini bilmasdan turib chatgptni to'liqona ishlatishingiz mumkin bo'ladi.
 
 """
-
-
-GROUP_INFO_COMMAND = """Shaxsiy va guruh suhbatlaringizda yordam beradigan foydali yordamchi! Ushbu botning <b>guruhda</b> ishlash tartibi quyidagicha:
-
-1️⃣ <b>Guruhga qo'shish</b>: MuloqotAIdan foydalanish uchun, uningni Telegram gruhingizga qo'shing. Bu uchun "@muloqataibot" ni qidiring va uningni gruhga taklif qiling.
-
-2️⃣ <b>Admin huquqlarini berish</b>: MuloqotAItning samarali ishlashi uchun uningni admin sifatida qo'shish kerak. Uningga to'g'ri admin huquqlarini berishni unutmang, masalan, xabarlarni o'chirish (ixtiyoriy) va boshqa sozlamalarni boshqarish.
-
-3️⃣ <b>Gruhda suhbatlashish</b>: Bot bilan suhbat qurish uchun unga reply tarzida so'rov yuboring. Guruh a'zolari savollarni so'rash, ma'lumot so'ralish, yordam so'ralish yoki qiziqarli suhbatlar olib borishlari mumkin.
-
-➕ <b>Ochiq guruh:</b> @muloqataigr"""
-
-
-ABILITY_COMMAND = """💡 Aqlli: Ko'plab mavzularni tushunish va javob berishga tayyorman. Umumiy bilimdan ma'lumotlarni qidirishga qadar, 
-sizga aniqligi va maqbul javoblarni taklif etishim mumkin.
-
-🧠 Dono: Men doimiy o'rganish va rivojlanishda, yangi ma'lumotlarga va foydalanuvchi bilan bo'lishuvlarga moslashishim mumkin. Aqlli muloqotlarni taklif etishim mumkin.
-
-🌄 Rassom: Mening yana bir qobilyatlarimdan biri bu rasm generatsiya qila olishim. Men sizga xar qanday turdagi ajoyib rasmlarni generatsiya qilib olib bera olaman
-
-⚙️ Avtotarjimon: Meni siz bilan o'zbek tilida yanada yahshiroq muloqot qila olishim uchun, avtotarjima funksiyasini ishlataman. Endi siz ingliz tilida qiynalib menga yozishingiz shart emas. Bu funksiya ixtiyoriy xoxlagan paytiz o'chirib qo'yishingiz mumkin."""
-
-
-NEW_MEMBER_TEXT = """👋 Assalomu alaykum! Mening ismim MuloqotAI 
-Men sizga yordam berish uchun yaratilgan aqlli chatbotman. 
-Bot ishlashi uchun menga administrator huquqlarini bering
-😊 Ko'proq foydam tegishi uchun /help kommandasini yuborib men bilan yaqinroq tanishib chiqing
-"""
-
-GREETINGS_TEXT = "Assalomu aleykum Men Muloqot AI man sizga qanday yordam bera olaman?"
 
 
 def buy_text(price):
